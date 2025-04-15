ae8e067bc07e3c89c364f82f4dc0b7928ea6a1ae
kindroach <kindroach@hotmail.com>
2025-04-15 18:58:46 +0800

    Rename AutoVectorization.

---
+++ auto_vectorization.cpp
@@ -0,0 +1,40 @@
+#include <immintrin.h>
+
+#include "common.h"
+
+template <typename T>
+T vector_dot(const std::vector<T>& a, const std::vector<T>& b)
+{
+    assert(a.size() == b.size());
+
+    T sum = 0;
+    for (size_t i = 0; i < a.size(); i++)
+    {
+        sum += a[i] * b[i];
+    }
+
+    return sum;
+}
+
+
+int main(int argc, char* argv[])
+{
+    int n{1024};
+    if (argc > 1)
+    {
+        n = std::stoi(argv[1]);
+    }
+
+    size_t loop{100000000};
+    if (argc > 2)
+    {
+        loop = std::stoi(argv[2]);
+    }
+
+    std::vector a(n, 2);
+    std::vector b(n, 3);
+
+    auto sum = measure_avg_time(loop, vector_dot<int>, a, b);
+
+    std::cout << "sum=" << sum << std::endl;
+}
--- common.h
+++ common.h
@@ -1,4 +1,4 @@
-#include <immintrin.h>
+#pragma once
 
 #include <cassert>
 #include <chrono>
@@ -27,36 +27,4 @@ auto measure_avg_time(size_t runs, Func &&f, Args &&... args)
     std::cout << "avg: " << total_duration.count() / runs << " seconds" << std::endl;
 
     return result;
-}
-
-template<typename T>
-T vector_dot(const std::vector<T> &a, const std::vector<T> &b) {
-    assert(a.size() == b.size());
-
-    T sum = 0;
-    for (size_t i = 0; i < a.size(); i++) {
-        sum += a[i] * b[i];
-    }
-
-    return sum;
-}
-
-
-int main(int argc, char *argv[]) {
-    int n{1024};
-    if (argc > 1) {
-        n = std::stoi(argv[1]);
-    }
-
-    size_t loop{100000000};
-    if (argc > 2) {
-        loop = std::stoi(argv[2]);
-    }
-
-    std::vector a(n, 2);
-    std::vector b(n, 3);
-
-    auto sum = measure_avg_time(loop, vector_dot<int>, a, b);
-
-    std::cout << "sum=" << sum << std::endl;
 }
